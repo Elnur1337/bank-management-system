72dd026442468a66184c47b19c4c8abbb08c9130
Elnur Bjelić <elnur.bjelic1337@gmail.com>
2024-01-17 13:02:26 +0100

    Created some tests in main, and changed type of main to int

--- main.cpp
+++ main.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
 #include <vector>
+#include "Employee.h"
 
-void main() {
+int main() {
+	try
+	{
+		Employee e("1234567890122", "", "", "", "");
+	}
+	catch (const std::invalid_argument& e)
+	{
+		std::cout << e.what();
+	}
 
-	return;
+	return 0;
 }
